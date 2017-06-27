2f869d79759d87774f2985c7371643887fde8a1f
Alex Bennett <kestralttr@gmail.com>
2017-06-26 20:05:06 -0700

    adds preliminary search and lookup of entered strings into the Places API and returns markers on map

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,4 @@
-function initMap() {
+function initMap(arg) {
 
   let myLatLng = {lat: 41.850033, lng: -87.6500523};
 
@@ -7,40 +7,53 @@ function initMap() {
     center: myLatLng
   });
 
+  let input;
+
+  if(arg === "") {
+    input = "resturant";
+  } else {
+    input = arg;
+  }
+
+  console.log(input);
+
   let request = {
     location: myLatLng,
     radius:'500',
-    types: ["store"]
+    types: [input]
   };
 
   service = new google.maps.places.PlacesService(myMap);
   service.nearbySearch(request, callback);
 
   function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (let i = 0; i < results.length; i++) {
-      let place = results[i];
-      console.log(place);
-      let lat = place.geometry.location.lat();
-      let lng = place.geometry.location.lng();
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        let place = results[i];
+        console.log(place.name);
+        let lat = place.geometry.location.lat();
+        let lng = place.geometry.location.lng();
 
-      new google.maps.Marker({
-        position: {lat,lng},
-        map: myMap,
-        title: place.name
-      });
+        new google.maps.Marker({
+          position: {lat,lng},
+          map: myMap,
+          title: place.name
+        });
 
+      }
     }
   }
-}
 
 }
 
 (function() {
   let searchButton = document.getElementById("search-button");
+  let queryInput = document.getElementById("query-input");
 
   searchButton.addEventListener("click", function(e) {
     e.preventDefault();
+    console.log(queryInput.value);
+    initMap(queryInput.value);
   });
 
 
