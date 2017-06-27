9f29059b81334fd0e14e42665c4244687f6ce387
Alex Bennett <kestralttr@gmail.com>
2017-06-26 19:41:04 -0700

    initial commit

---
+++ javascript/javascript.js
@@ -0,0 +1,47 @@
+function initMap() {
+
+  let myLatLng = {lat: 41.850033, lng: -87.6500523};
+
+  let myMap = new google.maps.Map(document.getElementById('googleMap'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  let request = {
+    location: myLatLng,
+    radius:'500',
+    types: ["store"]
+  };
+
+  service = new google.maps.places.PlacesService(myMap);
+  service.nearbySearch(request, callback);
+
+  function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      let place = results[i];
+      console.log(place);
+      let lat = place.geometry.location.lat();
+      let lng = place.geometry.location.lng();
+
+      new google.maps.Marker({
+        position: {lat,lng},
+        map: myMap,
+        title: place.name
+      });
+
+    }
+  }
+}
+
+}
+
+(function() {
+  let searchButton = document.getElementById("search-button");
+
+  searchButton.addEventListener("click", function(e) {
+    e.preventDefault();
+  });
+
+
+})();
